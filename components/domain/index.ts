e7de3c812d8d49d3175bb4f018afc283d31ea7cf
comppy <kim1122aass@gmail.com>
2026-05-22 23:21:36 +0900

    [Feat/#21]Feat: ParentListItem 컴포넌트 추가

--- components/domain/index.ts
+++ components/domain/index.ts
@@ -1,2 +1,4 @@
 export { RoleSelectCard } from './RoleSelectCard';
 export type { RoleSelectCardProps, RoleSelectCardVariant } from './RoleSelectCard';
+export { ParentListItem } from './ParentListItem';
+export type { ParentListItemProps } from './ParentListItem';
